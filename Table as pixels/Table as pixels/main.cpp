0c6a4050793070aa611db9b87d98fe035c4b5793
matizzx <matizerekx3@gmail.com>
2019-06-15 23:25:26 +0200

    pseudo paint

--- Table as pixels/Table as pixels/main.cpp
+++ Table as pixels/Table as pixels/main.cpp
@@ -5,6 +5,8 @@
 #include <cstdlib>
 #include <ctime>
 
+void start(SDL_Renderer *renderer, SDL_Rect red, SDL_Rect blue, SDL_Rect green);
+
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +20,10 @@ int main(int argc, char *argv[])
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
+	SDL_Rect red = { 0, 500, 100, 100 };
+	SDL_Rect blue = { 100, 500, 100, 100 };
+	SDL_Rect green = { 200, 500, 100, 100 };
+	start(renderer, red, blue, green);
 	while(IsRunning)
 	{
 		while (SDL_PollEvent(&event) != 0)
@@ -28,9 +34,6 @@ int main(int argc, char *argv[])
 				{
 					switch (event.key.keysym.sym)
 					{
-						default:
-							continue;
-
 						case(SDLK_ESCAPE):
 						{
 							IsRunning = 0;
@@ -43,31 +46,54 @@ int main(int argc, char *argv[])
 				{
 					if (IsRunning == 0)
 						break;
-					while (1)
+					while (IsRunning)
 					{
 						SDL_PollEvent(&event);
-						if(event.type == SDL_MOUSEBUTTONUP)
-							break;
-						SDL_SetRenderDrawColor(renderer, rand() % 255, rand() % 255, rand() % 255, 0);
-						SDL_RenderDrawPoint(renderer, event.button.x, event.button.y);
-						SDL_RenderPresent(renderer);
+						if (event.button.x <= 100 && event.button.y >= 500)
+							SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+						if (event.button.x > 100 && event.button.x <= 200 && event.button.y >= 500)
+							SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
+						if (event.button.x > 200 && event.button.x <= 300 && event.button.y >= 500)
+							SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+						if (event.key.keysym.sym == SDLK_ESCAPE || event.type == SDL_QUIT)
+						{
+							IsRunning = 0;
+						}
+						if (event.type == SDL_MOUSEBUTTONDOWN)
+						{
+							while (IsRunning)
+							{
+								SDL_PollEvent(&event);
+								if (event.type == SDL_MOUSEBUTTONUP)
+									break;
+								SDL_RenderDrawPoint(renderer, event.button.x, event.button.y);
+								SDL_RenderPresent(renderer);
+							}
+						}
 					}
 				}
 			}
 		}	
 
 		if (IsRunning == 0) 
-		{
 			break;
-		}
 		else
-		{
 			if (event.type == SDL_QUIT)
 				break;
-		}
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return EXIT_SUCCESS;
+}
+
+void start(SDL_Renderer *renderer, SDL_Rect red, SDL_Rect blue, SDL_Rect green)
+{
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 0);
+	SDL_RenderFillRect(renderer, &red);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
+	SDL_RenderFillRect(renderer, &blue);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 0);
+	SDL_RenderFillRect(renderer, &green);
+	SDL_RenderPresent(renderer);
 }
